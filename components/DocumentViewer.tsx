cb0f929f4b89566d9b0d47e76fc9450498ff8c1f
Abhi <abhinav118@gmail.com>
2025-08-19 16:21:48 -0400

    Add components/DocumentViewer.tsx

---
+++ components/DocumentViewer.tsx
@@ -0,0 +1,13 @@
+interface DocumentViewerProps {
+  size?: any
+  closable?: any
+  showFooter?: any
+}
+
+export default function DocumentViewer({ size, closable, showFooter }: DocumentViewerProps) {
+  return (
+    <div className="">
+      {/* DocumentViewer component content */}
+    </div>
+  )
+}
